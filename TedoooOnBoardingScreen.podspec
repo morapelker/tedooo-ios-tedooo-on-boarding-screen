5ed0ede38db7da3227798a9b8dc6e1f3cf2134c0
morapelker <morapelker@gmail.com>
2022-07-09 16:11:00 +0300

    "v1.0.3"

--- TedoooOnBoardingScreen.podspec
+++ TedoooOnBoardingScreen.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TedoooOnBoardingScreen'
-  s.version          = '1.0.2'
+  s.version          = '1.0.3'
   s.summary          = 'TedoooOnBoardingScreen'
 
 # This description is used to generate tags and improve search results.
--- TedoooOnBoardingScreen/Classes/TedoooOnBoardingScreen.swift
+++ TedoooOnBoardingScreen/Classes/TedoooOnBoardingScreen.swift
@@ -3,4 +3,5 @@ import CreateShopFlowApi
 
 public protocol TedoooOnBoardingScreen {
     func launchFlow(in viewController: UIViewController) -> AnyPublisher<AddShopResult, Never>
+    func launchFlow(inNavController navController: UINavigationController) -> AnyPublisher<AddShopResult, Never>
 }
