54137e745815cc56728f84d2c5f4a8782bc8924c
morapelker <morapelker@gmail.com>
2022-07-10 15:29:48 +0300

    "v1.0.4"

--- TedoooOnBoardingScreen/Classes/TedoooOnBoardingScreen.swift
+++ TedoooOnBoardingScreen/Classes/TedoooOnBoardingScreen.swift
@@ -1,7 +1,18 @@
 import Combine
 import CreateShopFlowApi
 
+public class FlowResult {
+    
+    public init(endPublisher: AnyPublisher<AddShopResult, Never>, onboardingComplete: AnyPublisher<Any?, Never>) {
+        self.endPublisher = endPublisher
+        self.onboardingComplete = onboardingComplete
+    }
+    
+    public let endPublisher: AnyPublisher<AddShopResult, Never>
+    public let onboardingComplete: AnyPublisher<Any?, Never>
+}
+
 public protocol TedoooOnBoardingScreen {
-    func launchFlow(in viewController: UIViewController) -> AnyPublisher<AddShopResult, Never>
-    func launchFlow(inNavController navController: UINavigationController) -> AnyPublisher<AddShopResult, Never>
+    func launchFlow(in viewController: UIViewController) -> FlowResult
+    func launchFlow(inNavController navController: UINavigationController) -> FlowResult
 }
